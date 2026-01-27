8711b19282a6d8fedd35f3ac04d8db8a599121df
liulihao <551772063@qq.com>
2026-01-27 11:11:11 +0800

    fix: a

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       port: VITE_PORT,
       host: "0.0.0.0",
       // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
-      proxy: createProxy(VITE_PROXY),
+      // proxy: createProxy(VITE_PROXY),
       // 预热文件以提前转换和缓存结果，降低启动期间的初始页面加载时长并防止转换瀑布
       warmup: {
         clientFiles: ["./index.html", "./src/{views,components}/*"]
