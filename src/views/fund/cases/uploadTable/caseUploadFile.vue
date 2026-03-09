d8b8a0d5a943e2cf20f8b048afac4a27dac443a9
liulihao <551772063@qq.com>
2026-03-09 15:47:43 +0800

    fix: 更新prettier

---
+++ src/views/fund/cases/uploadTable/caseUploadFile.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref, getCurrentInstance } from 'vue'
+const { proxy } = getCurrentInstance()
+const isShow = ref()
+const isShowInfo = ref()
+const uploadFileHandle = () => {
+  isShow.value = true
+}
+const showUploadInfo = () => {
+  isShowInfo.value = true
+}
+</script>
+
+<template>
+  <div>
+    <o-button @click="uploadFileHandle">上传文件</o-button>
+    <o-dialog ref="dialogRef" v-model="isShow" title="上传文件">
+      <template #title>
+        <div>上传文件</div>
+        <el-button type="primary" @click="showUploadInfo">上传文件格式说明</el-button>
+      </template>
+    </o-dialog>
+  </div>
+</template>
--- src/views/fund/cases/uploadTable/index.vue
+++ src/views/fund/cases/uploadTable/index.vue
@@ -1,75 +1,75 @@
 <script setup lang="ts">
-import { ref, getCurrentInstance } from "vue";
-import { getCasefileList } from "@/api/analysis.ts";
-import { getStorage } from "@oeos-components/utils";
-const { proxy } = getCurrentInstance();
+import { ref, getCurrentInstance } from 'vue'
+import { getCasefileList } from '@/api/analysis.ts'
+import { getStorage } from '@oeos-components/utils'
+const { proxy } = getCurrentInstance()
 
-const total = ref(0);
-const data = ref([]);
+const total = ref(0)
+const data = ref([])
 const init = async () => {
   let params = {
     pageNo: 1,
     pageSize: 10,
-    caseId: getStorage("caseId"),
-    folder: "",
-    fileName: ""
-  };
-  let res = await getCasefileList(params);
-  data.value = res.records ?? [];
-  total.value = res.total;
-};
-init();
+    caseId: getStorage('caseId'),
+    folder: '',
+    fileName: '',
+  }
+  let res = await getCasefileList(params)
+  data.value = res.records ?? []
+  total.value = res.total
+}
+init()
 
-const editRow = row => {};
+const editRow = (row) => {}
 const columns = [
   {
-    label: "源文件",
-    prop: "sourceFile"
+    label: '源文件',
+    prop: 'sourceFile',
   },
   {
-    label: "文件夹",
-    prop: "folder"
+    label: '文件夹',
+    prop: 'folder',
   },
   {
-    label: "状态",
-    prop: "status"
+    label: '状态',
+    prop: 'status',
   },
   {
-    key: "operation",
-    label: "操作",
+    key: 'operation',
+    label: '操作',
     width: 200,
     btns: [
       {
-        content: "字段映射",
-        handler: textRow
+        content: '字段映射',
+        handler: textRow,
       },
       {
-        content: "转换查看",
-        handler: translateRow
+        content: '转换查看',
+        handler: translateRow,
       },
       {
-        content: "删除",
+        content: '删除',
         handler: deleteRow,
-        comp: "o-icon",
+        comp: 'o-icon',
         attrs: {
-          name: "delete",
-          type: "svg",
-          content: "删除"
-        }
-      }
-    ]
-  }
-];
+          name: 'delete',
+          type: 'svg',
+          content: '删除',
+        },
+      },
+    ],
+  },
+]
 
 function translateRow(row) {
-  console.log(`37 row`, row);
+  console.log(`37 row`, row)
 }
 function textRow(row) {
-  console.log(`97 row`, row);
+  console.log(`97 row`, row)
 }
 
 function deleteRow(row) {
-  console.log(`46 row`, row);
+  console.log(`46 row`, row)
 }
 </script>
 
