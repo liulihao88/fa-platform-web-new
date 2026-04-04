894c5c09d4587a429cf5f9ace12a00803fca78f4
liulihao <551772063@qq.com>
2026-04-04 18:48:41 +0800

    fix: a

--- src/views/operations/orgConfig/orgDialog.vue
+++ src/views/operations/orgConfig/orgDialog.vue
@@ -69,7 +69,7 @@ defineExpose({
 
 <template>
   <o-dialog v-model="isShow" :title="title" width="720px" :showConfirm="!readOnly" @confirm="save">
-    <el-form ref="formRef" :model="form" :rules="rules" label-width="110px">
+    <el-form ref="formRef" :model="form" :rules="rules" label-width="auto">
       <el-form-item label="机构官方名称" prop="orgName">
         <o-input v-model="form.orgName" :disabled="readOnly" />
       </el-form-item>
